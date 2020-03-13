150c280bc066657c84f04e8b6d886ead27eed370
cedric-winbush <cawinbushjr@gmail.com>
2020-03-13 10:14:47 -0500

    configure npm script for server and start to execute node.  set up initial get to make sure it is connected --working

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-
+const cors = require('cors')
 
 const server = express();
 
+
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({message: "It is working"})
+})
 
 
 module.exports = server;
