e757ab11a0f683cb7cccb43cdbd2a010d846766a
cedric-winbush <cawinbushjr@gmail.com>
2020-03-13 10:30:45 -0500

    create GET for all projects ---working

--- data/routers/actionsRouter.js
+++ data/routers/actionsRouter.js
@@ -4,4 +4,18 @@ const router = require('express').Router();
 actionsDB = require('../helpers/actionModel');
 
 
+//GET All Actions
+
+
+//GET Actions by :id
+
+
+
+// POST 
+
+
+// PUT by :id
+
+
+// DELETE by :id
 module.exports = router;
--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -2,4 +2,31 @@ const router = require('express').Router();
 
 const projectsDB = require('../helpers/projectModel');
 
+//GET All Projects
+router.get('/', (req, res) => {
+
+  projectsDB.get().then(projects => {
+    res.status(200).json(projects)
+  }).catch(err => {
+    res.status(500).json({ error: `There was an error: ${err}`})
+  })
+})
+
+
+//GET Projects by :id
+
+
+//GET Actions by project :id
+
+
+// POST 
+
+
+// PUT by :id
+
+
+// DELETE by :id
+
+
+
 module.exports = router;
